0cfd2e9e65611a3f51edb5af4881a45b25e31c7f
masami <masami.dev@gmail.com>
2016-01-24 01:13:07 +0900

    トリップキーに&<>◆などが含まれていると正しいトリップがプレビューに表示されない現象を修正

--- chaika/chrome/content/chaika/post/post.js
+++ chaika/chrome/content/chaika/post/post.js
@@ -162,19 +162,18 @@ Post.prototype = {
         preview["vlinkColor"] = getSetting("BBS_VLINK_COLOR");
 
 
-        var name = convertEntity(this.name || getSetting("BBS_NONAME_NAME") || "");
-        name = name.replace("◆", "◇", "g");
-
+        var name = this.name || getSetting("BBS_NONAME_NAME") || "";
 
         // トリップ変換
-        var tripPos = name.indexOf("#");
+        var nameKey = name.match(/^(.*?)(#.*)?$/);
+        var tripKey = nameKey[2];
+        name = convertEntity(nameKey[1]).replace(/◆/g, "◇");
 
-        if(tripPos !== -1){
-            let tripKey = name.substring(tripPos);
+        if(tripKey){
             let trip = Trip.getTrip(tripKey);
 
             name = [
-                name.substring(0, tripPos),
+                name,
                 " <span class='resSystem'>",
                 "◆",
                 trip,
