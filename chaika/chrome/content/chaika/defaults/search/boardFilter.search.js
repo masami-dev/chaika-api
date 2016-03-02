dae8ff67483004f72e9511a5a42377553579e023
masami <masami.dev@gmail.com>
2016-03-03 00:01:10 +0900

    板名フィルタの絞り込み検索で大文字小文字や全角半角などを区別しないようにした
    全角半角はまだしも大文字小文字も区別されてしまうと、絞り込み検索としては
    ちょっと使いづらいかなと感じたので、1.7.3 までとほぼ同じなるように変更しました。
    
    Unicode 正規化には Firefox 31 から利用できる String#normalize を使っています。
    板名だけでなくカテゴリ名も結果に含まれてしまう現象も併せて修正しました。
    
    1.7.3 までの板名フィルタでは、SQL の LIKE 演算子のワイルドカード（ _ と % ）
    を使った検索もできるのですが、これを利用していた人はいないと思われるので、
    ワイルドカード検索までは実装していません。

--- chaika/chrome/content/chaika/defaults/search/boardFilter.search.js
+++ chaika/chrome/content/chaika/defaults/search/boardFilter.search.js
@@ -3,23 +3,38 @@
 Components.utils.import("resource://chaika-modules/ChaikaBBSMenu.js");
 
 
+/**
+ * Polyfill for Firefox 39-
+ */
+if(!String.prototype.includes){
+    String.prototype.includes = function(){'use strict';
+        return String.prototype.indexOf.apply(this, arguments) !== -1;
+    };
+}
+
+
 var BoardFilter = {
 
     id: '99.chaika.filter.board',
 
     name: '板名フィルタ',
 
-    version: '2.0.0',
+    version: '2.0.1',
 
     charset: 'utf-8',
 
     url: null,
 
     search: function(query){
-        return ChaikaBBSMenu.getXML().then((xml) => {
-            let results = xml.querySelectorAll('[title*="' + query + '"]');
+        query = query.toLowerCase().normalize('NFKC');
 
-            return Array.from(results).map((node) => {
+        return ChaikaBBSMenu.getXML().then((xml) => {
+            let results = Array.from(xml.querySelectorAll('board[title]')).filter((node) => {
+                let title = node.getAttribute('title').toLowerCase().normalize('NFKC');
+                return title.includes(query);
+            });
+
+            return results.map((node) => {
                 return {
                     title: node.getAttribute('title'),
                     url: node.getAttribute('url')
