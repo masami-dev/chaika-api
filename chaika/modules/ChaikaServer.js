1e663f3d380439bad245108e15a23f05a57d0a8a
masami <masami.dev@gmail.com>
2017-03-13 16:32:39 +0900

    ChaikaServer.js で HTTP Request が来ずに切断された場合はエラー扱いしないようにした
    Connect されたもののクライアント側から何も送信されて来ないまま接続が切れた場合、
    今までは 400 Invalid Request の扱いになり、
    ブラウザコンソールのエラーログに NS_BASE_STREAM_CLOSED が表示されていたが、
    このケースをエラー扱いにせず正常終了として扱うように変更した。
    
    現時点の Firefox では、HTML の <a href=""> リンクの上にマウスカーソルが乗った時点で
    リンク先のサーバーへ Connect を行い、クリックされた時点で HTTP Request を送るという
    仕組みになっているようである。
    
    なので、Default スキンなどで >>100 のようなレスアンカーにマウスカーソルを乗せて
    レスポップアップを表示させただけの場合でも、ChaikaServer.js へ Connect されるが
    HTTP Request は送られずに数秒後に切断されるという動作が発生する。

--- chaika/modules/ChaikaServer.js
+++ chaika/modules/ChaikaServer.js
@@ -182,6 +182,13 @@ ChaikaServerHandler.prototype = {
         try{
             this.request = new ChaikaServerRequest(aInputStream);
         }catch(ex){
+            if(ex.result == Cr.NS_BASE_STREAM_CLOSED){
+                // 接続はされたが何も送られて来ずに切断された場合
+                // nsIInputStream.available() が投げてくる例外を検知して
+                // エラー扱いにはせずに終了する
+                this.close();
+                return;
+            }
             ChaikaCore.logger.error(ex);
 
             aInputStream.close();
