d0f4bc3afe455eeb1ae4d708e717224076d7a539
masami <masami.dev@gmail.com>
2017-05-17 01:17:57 +0900

    2ch.net でサーバ移転を検知できないケースが出てきたのを修正
    302 で「人大杉」へのリダイレクトではなく、301 or 302 で板移転先の
    subject.txt へのリダイレクトを返してくるケースが出てきた。
    
    スクレイピングで移転先を検知する今までの方法より、
    このときの Location: を移転先として検知した方がいいかもしれない。

--- chaika/chrome/content/chaika/board/page.js
+++ chaika/chrome/content/chaika/board/page.js
@@ -520,7 +520,7 @@ function subjectUpdate(aEvent, aForce){
 		var subjectFile = gBoard.subjectFile.clone();
 		var settingFile = gBoard.settingFile.clone();
 
-		if(aStatus == 302 || !subjectFile.exists() || subjectFile.fileSize==0){
+		if(aStatus == 301 || aStatus == 302 || !subjectFile.exists() || subjectFile.fileSize==0){
 			setStatus("スレッド一覧を取得できませんでした。板が移転した可能性があります。");
 			document.getElementById("dckUpdate").selectedIndex = 1;
 			return;
